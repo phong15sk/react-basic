c2807ede77e164a732c0dfcd48582629efa7f4cd
phong15sk <vovanphong15sk@gmail.com>
2023-05-20 18:40:40 +0700

    Add user

--- src/components/Example/ChildComponent.js
+++ src/components/Example/ChildComponent.js
@@ -2,12 +2,27 @@ import React from "react";
 
 class ChildComponent extends React.Component {
   render() {
-    console.log(this.props);
+    console.log("ChildComponent", this.props);
     return (
       <>
         <div>
           Hello {this.props.user.firstName} {this.props.user.lastName}
         </div>
+        <table>
+          <tr>
+            <th>STT</th>
+            <th>FistName</th>
+            <th>LastName</th>
+          </tr>
+
+          {this.props.arrayUser.map((x, index) => (
+            <tr key={index}>
+              <td>{index}</td>
+              <td>{x.firstName}</td>
+              <td>{x.lastName}</td>
+            </tr>
+          ))}
+        </table>
       </>
     );
   }
